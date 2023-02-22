0cccc2d3079a0c920a8cc4336867a9dbcd2c75a8
srishtisisodia <srishti.sisodia1603@gmail.com>
2023-02-22 19:48:44 +0530

    changing port

--- client/script.js
+++ client/script.js
@@ -83,7 +83,7 @@ const handleSubmit = async (e) => {
     // messageDiv.innerHTML = "..."
     loader(messageDiv);
 
-    const response = await fetch('https://chatgptapp-lswi.onrender.com', {
+    const response = await fetch('https://chatgptapp-lswi.onrender.com/', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
